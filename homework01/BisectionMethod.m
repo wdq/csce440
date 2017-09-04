14d1a18ee86181e96a9c2bd1367d6f8649bb2c23
William Quade <liam@quade.co>
2017-09-04 08:38:29 -0500

    Working BisectionMethod code.

--- homework01/BisectionMethod.m
+++ homework01/BisectionMethod.m
@@ -1,23 +1,28 @@
-function [ output ] = BisectionMethod( equation, intervalStart, intervalEnd, tolerance, maxIterations )
+% Find the approximate root of a function using the Bisection Method.
+
+% Example usage:
+% f = @(x) (4*(x^3))-(x^2)+(8*x)+(4*(exp(x)))+10
+% out = BisectionMethod(f, -1, 1, 50, 0.0001)
+
+function [ output ] = BisectionMethod( equation, intervalStart, intervalEnd, maxIterations, tolerance )
+    
+    % Keep track of iteration number, previous approximation, and interval.
     iteration = 0;
     a = intervalStart;
     b = intervalEnd;
     previousP = 0;
     
-    % Todo: It looks like the method requires checking for the stopping
-    % criteria and then determining to run (f,a,p) or (f,p,b) before it
-    % starts running.
-    
+    % Try while below the maximum number of iterations.
     while iteration < maxIterations
         
-        
         % Find the function output values and midpoints that are needed
-        p = (intervalStart+intervalEnd)/2;
+        p = (a+b)/2;
         pOutput = equation(p);
         aOutput = equation(a);
         bOutput = equation(b);
         
-        fprintf('Iteration #%d: BisectionMethod(f, %d, %d), p=%d\n', iteration, a, b, p);
+        % Print out iteration info
+        fprintf('Iteration #%d: BisectionMethod(f, %f, %f), p=%f\n', iteration, a, b, p);
         
         % Check stopping criteria conditions
         stoppingCriteriaMet = 0;
@@ -50,9 +55,5 @@ function [ output ] = BisectionMethod( equation, intervalStart, intervalEnd, tol
         previousP = p;
         iteration = iteration + 1;
     end
-    
-    
+    output = 'Error: reached iteration limit without finding approximation within tolerance.'
 end
-
-% f = @(x) (4*(x^3))-(x^2)+(8*x)+(4*(exp(x)))+10
-% out = BisectionMethod(f, -1, 1, 0.0001, 100000)
